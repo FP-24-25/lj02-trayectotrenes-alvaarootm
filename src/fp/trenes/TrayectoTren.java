6009b6d70d1eacd2cb3be2d98592b0df647bbd7d
alvaarootm <alvarotm006@gmail>
2025-03-06 19:11:40 +0100

    Actualizar2

---
+++ src/fp/trenes/TrayectoTren.java
@@ -0,0 +1,18 @@
+package fp.trenes;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface TrayectoTren {
+	String getCodigoTren();
+	String getNombre();
+	TipoTren getTipo();
+	List<String> getEstaciones();
+	List<LocalTime> getHorasSalida();
+	List<LocalTime> getHorasLlegada();
+	LocalTime getHoraSalida();
+	LocalTime getHoraLlegada();
+	Duration getDuracion();
+
+}
